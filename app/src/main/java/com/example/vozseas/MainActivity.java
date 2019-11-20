6688df70a58bf754cbe9e9136177a095cdfdbfab
Ander Maestre <andermaestre_90@hotmail.es>
2019-11-20 19:34:45 +0100

    Proyecto personal

---
+++ app/src/main/java/com/example/vozseas/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.vozseas;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    ImageButton btnHablar;
+    TextView txtTraduccion;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txtTraduccion=findViewById(R.id.textView);
+        btnHablar=findViewById(R.id.btnSpeak);
+        btnHablar.setOnClickListener(this);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch(requestCode){
+            case 10:
+                if(resultCode == RESULT_OK && data!=null){
+                    data.getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+                }
+
+
+                break;
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault());
+        if(intent.resolveActivity(getPackageManager())!= null){
+            startActivityForResult(intent,10);
+        }else{
+            Toast.makeText(this,"No tienes poder suficiente, farmea y vuelve",Toast.LENGTH_LONG).show();
+        }
+
+
+    }
+}
